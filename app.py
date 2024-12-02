5e10418bec41b5cd52bf1cff5b250b8d7215c2d0
ankitprajapat42 <ankitprajapat9414@gmail.com>
2024-12-02 21:18:17 +0530

    update index.html, style.css

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method=='POST':
+        data=request.form.get('url')
+    else:
+        return render_template('index.html')
 
 if __name__=='__main__':
     app.run('localhost',100,debug=True)
